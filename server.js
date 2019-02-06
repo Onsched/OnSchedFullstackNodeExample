bdaf54241f6e756df15c3439992d65c63eeeb9f1
Michael Reeves <md.reeves@icloud.com>
2019-02-06 09:45:25 -0700

    Stub out initial Express backend in server.js

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
-console.log('Hello World')
+// server.js - this file is used to launch the Express server
+// and sets up the listener on port 5000 for development
+const express = require('express')
+
+
+// configure the PORT which the app will use to 
+// listen for incoming traffic
+const PORT = process.env.PORT || 5000
+
+
+// instantiate the express application
+// the app object is used to configure the
+// server, listen to incoming request, and
+// route them to the appropriate route handlers
+const app = express()
+
+
+//-------------------------
+// Route Handlers
+//-------------------------
+app.get('/', 
+  (request, response) => {
+    response.send( { hi: 'there' } )
+  }
+)
+
+
+//-------------------------
+// set up server listening
+//-------------------------
+app.listen( PORT, 
+            () => console.log(`Listening on http://localhost:${PORT}`) 
+)
 
