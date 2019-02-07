d4f21066fbc09a1a13d468fa811ec628e82c52cf
Michael Reeves <md.reeves@icloud.com>
2019-02-07 16:18:21 -0700

    Add morgan to the project to output request traffic

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 // server.js - this file is used to launch the Express server
 // and sets up the listener on port 5000 for development
 const express = require('express')
+const morgan  = require('morgan')
 
 
-// configure the PORT which the app will use to 
-// listen for incoming traffic
+//----------------------
+// Configuration
+//----------------------
+// if NODE_ENV is undefined, this is development
+const isDevelopment = !(process.env.NODE_ENV)
+
+// configure the HOST & PORT which the app will 
+// use to listen for incoming traffic
 const HOST = process.env.HOST || 'localhost'
 const PORT = process.env.PORT || 5000
+
 
 // instantiate the express application
 // the app object is used to configure the
 // server, listen to incoming request, and
 // route them to the appropriate route handlers
 const app = express()
+
+
+//-----------------------
+// Server Middleware
+//-----------------------
+// each call to app.use wires up a piece of middleware
+// inside the application.  these middleware modify
+// incoming requests to the app BEFORE they are sent to
+// the route handlers
+
+// if isDevelopment, add console output
+if ( isDevelopment ) {
+  app.use( morgan('dev') )
+}
 
 
 //-------------------------
