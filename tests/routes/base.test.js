9bc44ad27a481f0226fcc9f9c4c31350e64439e5
Michael Reeves <md.reeves@icloud.com>
2019-02-08 10:57:52 -0700

    Add Unit Testing setup
    - Separate Express configuration (app.js) from initialization
      (server.js).
    - Initialize jest with jest.config.js
    - Add initial base unit test for / route
    - Update .gitignore and .dockerignore to ignore test output
    - Add jest-junit and supertest to dev dependencies in package.json
    - Add jest-junit configuration to package.json
    - Add CI test configuration to scripts section of package.json
    - Add dev script with nodemon to scripts section of package.json

---
+++ tests/routes/base.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest')
+
+const app = require('../../app')
+
+
+describe( 'Test base routes', () => {
+
+  describe( 'GET /', () => {
+
+    test( 'should respond with success', async () => {
+      const response = await request( app ).get( '/' )
+
+      expect( response.statusCode ).toEqual( 200  )
+      expect( response.body       ).toEqual( { hi: "there" } )
+
+    })
+
+  })
+
+})
+
