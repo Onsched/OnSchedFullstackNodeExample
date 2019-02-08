0c814ce6db4c958b2ff0e539efa1e71f6f77cecc
Michael Reeves <md.reeves@icloud.com>
2019-02-08 10:57:52 -0700

    Add Unit Testing setup
    - Separate Express configuration (app.js) from initialization
      (server.js).
    - Initialize jest with jest.config.js
    - Add initial base unit test for / route
    - Update .gitignore and .dockerignore to ignore test output
    - Add jest-junit and supertest to dev dependencies in package.json
    - Add jest-junit configuration to package.json
    - Add CI test configuration to scripts section of package.json
    - Add dev script with nodemon to scripts section of package.json

---
+++ app.js
@@ -0,0 +1,50 @@
+// Express server configuration
+// Configuration of the app has been separated
+// from the actual server initialization
+// (server.js) to support testing
+const express = require('express')
+const morgan  = require('morgan')
+
+
+//----------------------
+// Configuration
+//----------------------
+// if NODE_ENV is undefined, this is development
+const isDevelopment = !(process.env.NODE_ENV)
+
+
+// instantiate the express application
+// the app object is used to configure the
+// server, listen to incoming request, and
+// route them to the appropriate route handlers
+const app = express()
+
+
+//-----------------------
+// Server Middleware
+//-----------------------
+// each call to app.use wires up a piece of middleware
+// inside the application.  these middleware modify
+// incoming requests to the app BEFORE they are sent to
+// the route handlers
+
+// if isDevelopment, add console output
+if ( isDevelopment ) {
+  app.use( morgan('dev') )
+}
+
+
+//-------------------------
+// Route Handlers
+//-------------------------
+app.get('/', 
+  (request, response) => {
+    response.send( { hi: 'there' } )
+  }
+)
+
+
+// export the app for testing and 
+// use in server.js
+module.exports = app
+
--- server.js
+++ server.js
@@ -1,50 +1,13 @@
-// server.js - this file is used to launch the Express server
-// and sets up the listener on port 5000 for development
-const express = require('express')
-const morgan  = require('morgan')
+// server.js - this file is used to launch the
+// Express server and sets up the listener on
+// http://localhost:5000 for development
+const app = require('./app')
 
 
-//----------------------
-// Configuration
-//----------------------
-// if NODE_ENV is undefined, this is development
-const isDevelopment = !(process.env.NODE_ENV)
-
-// configure the HOST & PORT which the app will 
-// use to listen for incoming traffic
+// configure the app to listen for incoming
+// traffic on HOST and PORT
 const HOST = process.env.HOST || 'localhost'
 const PORT = process.env.PORT || 5000
-
-
-// instantiate the express application
-// the app object is used to configure the
-// server, listen to incoming request, and
-// route them to the appropriate route handlers
-const app = express()
-
-
-//-----------------------
-// Server Middleware
-//-----------------------
-// each call to app.use wires up a piece of middleware
-// inside the application.  these middleware modify
-// incoming requests to the app BEFORE they are sent to
-// the route handlers
-
-// if isDevelopment, add console output
-if ( isDevelopment ) {
-  app.use( morgan('dev') )
-}
-
-
-//-------------------------
-// Route Handlers
-//-------------------------
-app.get('/', 
-  (request, response) => {
-    response.send( { hi: 'there' } )
-  }
-)
 
 
 //-------------------------
