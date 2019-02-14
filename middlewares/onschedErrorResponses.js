ebeaff07f7567fb63eb88749f0a728d87dceea26
Michael Reeves <md.reeves@icloud.com>
2019-02-14 12:18:37 -0700

    Add /auth/initalize route
    - Add axios and lodash to project
    - Add routes/auth.js to handle authentication routes
    - Use Express router to handle auth routes in app.js and auth.js
    - Return access_token from /auth/initialize when successful
    - Add onschedErrorResponses middleware to handle error responses
    - Move isDevelopment into config/dev.js
    - Response with a Welcome message to / route

---
+++ middlewares/onschedErrorResponses.js
@@ -0,0 +1,95 @@
+// custom error handling for OnSched API
+const _ = require('lodash')
+
+const keys = require('../config/keys')
+
+
+module.exports = ( error, request, response, next ) => {
+  if ( response.headersSent ) {
+    return next( error )
+  }
+
+  const responseObj   = {}
+  const errorResponse = error.response           || {}
+  const status        = errorResponse.status     || 500
+  const statusText    = errorResponse.statusText || 'Server Error'
+  const data          = errorResponse.data       || 'An unknown error has occurred'
+
+  // create a JSON API style response object
+  //
+  // handle '401: Unauthorized error'
+  if ( errorResponse && status === 401 ) {
+    responseObj.errors = [
+      {
+        status: status,
+        title:  statusText
+      }
+    ]
+
+  }
+  // if there is only a single error response on the
+  // data.error property
+  else if ( errorResponse && data && data.error ) {
+    responseObj.errors = [
+      {
+        status: status,
+        title:  statusText,
+        detail: data.error
+      }
+    ]
+
+  }
+  // if the errorResponse has a data.message property
+  // handle a different error response
+  else if ( errorResponse && data && data.message ) {
+    responseObj.errors = [
+      {
+        status: status,
+        title:  statusText,
+        detail: data.message
+      }
+    ]
+  }
+  // handle additional field errors
+  else if ( errorResponse && _.size( data ) > 0 )
+  {
+    const errorsArray = []
+
+    _.forOwn( errorResponse.data, (value, key) => {
+      errorsArray.push(
+        {
+          source: { parameter: key },
+          status: status,
+          title:  statusText,
+          detail: value.join(', ')
+        }
+      )
+
+    })
+
+    responseObj.errors = errorsArray
+
+  }
+  // else error is undetermined.
+  // generate a generic response
+  else {
+    responseObj.errors = [
+      {
+        status: status,
+        title:  statusText,
+        detail: data
+      }
+    ]
+
+  }
+
+  if ( keys.isDevelopment ) {
+    console.log( error.stack )
+  }
+
+  // send the error response to the caller
+  response.status( status )
+          .json( responseObj )
+
+}
+
---
+++ routes/auth.js
@@ -0,0 +1,54 @@
+const express     = require('express')
+const axios       = require('axios')
+const querystring = require('querystring')
+
+const keys           = require('../config/keys')
+const errorResponses = require('../middlewares/onschedErrorResponses')
+
+
+// initialize the router
+const router = express.Router()
+
+
+//-------------------------
+// Route Handlers
+//-------------------------
+router.get( '/initialize', 
+  (request, response, next) => {
+    const params = {
+      scope:    'OnSchedApi',
+      grant_type: 'client_credentials',
+      client_id:  keys.onschedClientID,
+      client_secret: keys.onschedClientSecret
+    }
+
+    const tokenURL = `${keys.onschedIdentityURL}/connect/token`
+
+    // querystring.stringify(params) will encode the param data
+    // in application/x-www-urlencoded format, which is required
+    // by the Identity Server
+    axios.post( tokenURL, querystring.stringify(params) )
+         .then( resp => {
+           // pass the access token back to the client app
+           response.send( resp.data ) || false
+         })
+         // if not, pass the error to the 
+         // onschedErrorResponse middleware
+         .catch( next )
+
+  }
+
+)
+
+
+//-------------------------------------------
+// route specific error handling middleware
+// error middleware must come after routes
+// and all other middleware
+//-------------------------------------------
+router.use( errorResponses )
+
+
+// export the router
+module.exports = router
+
