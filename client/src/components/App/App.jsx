a4aa48457a0033a63d306278c6416d33ab857a00
Michael Reeves <md.reeves@icloud.com>
2019-02-19 18:36:43 -0700

    Refactor client App into components directory
    - Move client App.js to Components/App/App.jsx
    - Move client App.css to Components/App/App.css
    - Move client App.test.js to Compoents/App/App.test.js
    - Export App class from App.jsx to enable testing when redux is
      introduced
    - Simplify JSX in App.jsx
    - Remove React logo.svg

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import './App.css'
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>
+            Welcome to OnSched!
+          </h2>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
